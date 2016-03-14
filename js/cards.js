a104ba9ac88a6f321d1ed3eaf807f24f1b2185a6
Abigail Rolling and Brad London <london.brad@gmail.com>
2016-03-14 15:12:50 -0700

    Create methods to generate randomized deck of cards

--- js/cards.js
+++ js/cards.js
@@ -1,13 +1,49 @@
-exports.Card = function(x, y, face) {
+function Card(x, y, face) {
   this.x = x;
   this.y = y;
   this.back = "glyphicon-picture";
   this.face = face;
 };
 
-exports.Card.prototype.location = function() {
+Card.prototype.location = function() {
   var coordinates = [];
   coordinates.push(this.x);
   coordinates.push(this.y);
   return coordinates;
-}
+};
+
+var randomFaces = function(totalCards){
+  var faces = ['glyphicon-home', 'glyphicon-trash', 'glyphicon-xbt', 'glyphicon-stats', 'glyphicon-credit-card', 'glyphicon-floppy-disk', 'glyphicon-cd', 'glyphicon-tree-deciduous', 'glyphicon-link', 'glyphicon-tower', 'glyphicon-globe', 'glyphicon-paperclip', 'glyphicon-send', 'glyphicon-education', 'glyphicon-grain', 'glyphicon-scale', 'glyphicon-ice-lolly', 'glyphicon-scissors', 'glyphicon-queen', 'glyphicon-tent', 'glyphicon-apple'];
+
+  var chosenPic = [];
+  for (var i = 0; i < (totalCards/2); i ++ ) {
+    var randomPos = Math.floor(Math.random() * faces.length );
+    chosenPic.push(faces[randomPos]);
+    chosenPic.push(faces[randomPos]);
+    faces.splice(randomPos, 1);
+  }
+  chosenPic.sort(function() {
+    return 0.5 - Math.random();
+  });
+  return chosenPic;
+};
+
+exports.createDeck = function(rows, columns) {
+  var deck = [];
+  if(rows % 2 === 1 && columns % 2 === 1) {
+    rows++;
+  }
+  var totalCards = rows * columns;
+  if(totalCards > 40) {
+    return false;
+  }
+  var allFaces = randomFaces(totalCards);
+  for (var i = 1; i <= rows; i++){
+    for(var j = 1; j <= columns; j++) {
+      var card = new Card(i, j, allFaces.pop());
+      deck.push(card);
+    }
+  }
+  return deck;
+
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,15 @@
 var expect = require('chai').expect;
-var Card = require('./../js/cards.js').Card;
+var createDeck = require('./../js/cards.js').createDeck;
 
 describe('Card', function() {
   it('will create a new card object with a place and face', function() {
-    var testCard = new Card(1, 2, "glyphicon-xbt");
-    expect(testCard.face).to.equal("glyphicon-xbt");
-    expect(testCard.location()).to.eql([1, 2]);
+    var testDeck = createDeck(3, 6);
+    expect(testDeck[7].back).to.equal("glyphicon-picture");
+    expect(testDeck[1].location()).to.eql([1, 2]);
+  });
+
+  it('will create a deck of sufficient size for x and y rows and columns', function() {
+    var testDeck = createDeck(3, 3);
+    expect(testDeck.length).to.equal(12);
   });
 });
